4518b07eee5a4e04409f4459d9cfeed01291c495
choubung <choubung@sookmyung.ac.kr>
2025-11-05 14:16:37 +0900

    first commit

---
+++ openMission/src/test/java/com/precourse/openMission/OpenMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precourse.openMission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/precourse/openMission/OpenMissionApplication.java
@@ -0,0 +1,13 @@
+package com.precourse.openMission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenMissionApplication.class, args);
+	}
+
+}
