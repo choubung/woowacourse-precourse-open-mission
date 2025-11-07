7d65fef66f4699768e8bd731e33d9baa31050271
choubung <choubung@sookmyung.ac.kr>
2025-11-07 20:21:14 +0900

    test(memo): memo > update 메소드 테스트 작성

--- src/test/java/com/precourse/openMission/domain/MemoTest.java
+++ src/test/java/com/precourse/openMission/domain/MemoTest.java
@@ -1,4 +1,47 @@
 package com.precourse.openMission.domain;
 
+import com.precourse.openMission.domain.memo.Memo;
+import com.precourse.openMission.domain.memo.MemoScope;
+import com.precourse.openMission.domain.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MemoTest {
+    // TODO: 업데이트 메소드 단위 테스트
+    private Memo memo;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        memo = Memo.builder()
+                .user(user)
+                .content("test")
+                .scope(String.valueOf(MemoScope.ONLY_ME))
+                .memoDate(LocalDateTime.of(2025, Month.NOVEMBER, 7, 15, 30))
+                .build();
+    }
+
+    @DisplayName("본문, 공개범위, 날짜를 업데이트하는 테스트")
+    @Test
+    void update_테스트(){
+        // given
+        String updatedContent = "updated test";
+        MemoScope updatedScope = MemoScope.EVERYONE;
+        LocalDateTime updatedDate = LocalDateTime.of(2025, Month.NOVEMBER, 10, 15, 30);
+
+        // when
+        memo.update(String.valueOf(updatedScope),updatedContent, updatedDate);
+
+        // then
+        assertThat(memo.getContent()).isEqualTo(updatedContent);
+        assertThat(memo.getScope()).isEqualTo(updatedScope);
+        assertThat(memo.getMemoDate()).isEqualTo(updatedDate);
+    }
 }
