db5dd4a43386bb8fe3a658a635359f7fad434b4e
choubung <choubung@sookmyung.ac.kr>
2025-11-08 15:36:10 +0900

    refactor(application): @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/precourse/openMission/OpenMissionApplication.java
+++ src/main/java/com/precourse/openMission/OpenMissionApplication.java
@@ -1,11 +1,13 @@
 package com.precourse.openMission;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OpenMissionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OpenMissionApplication.class, args);
 	}
