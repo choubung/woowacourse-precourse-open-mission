a42ebc27a60fbdb84e9920c9b1e726a6a195f7e4
choubung <choubung@sookmyung.ac.kr>
2025-11-12 22:00:28 +0900

    fix(user): 회원 탈퇴 메소드에 @Transactional 어노테이션 추가

--- src/main/java/com/precourse/openMission/service/UserService.java
+++ src/main/java/com/precourse/openMission/service/UserService.java
@@ -1,7 +1,7 @@
 package com.precourse.openMission.service;
 
 import com.precourse.openMission.config.auth.dto.SessionUser;
-import com.precourse.openMission.domain.memo.MemoRepository;
+import org.springframework.transaction.annotation.Transactional;
 import com.precourse.openMission.domain.user.User;
 import com.precourse.openMission.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional
     public void deleteUser(SessionUser sessionUser) {
         User user = userRepository.findByEmail(sessionUser.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
